dc728acf979a5d95a658d35cc33bba3c48d8414c
greysvarlesp <tr4nquoc@gmail.com>
2023-01-01 14:03:03 +0700

    next

--- src/host.cu
+++ src/host.cu
@@ -1,5 +1,34 @@
 #include <stdio.h>
 
+
+const int SOBEL_X[] = {
+  1, 0, -1, 
+  2, 0, -2, 
+  1, 0, -1,
+};
+
+const int SOBEL_Y[] = {
+  1, 2, 1,
+  0 ,0 ,0,
+  -1, -2, -1
+};
+
+
+/*
+Input: n * m grayscale image
+Output: n * m energy map
+*/
+int* sobel_conv(int *in, int n, int m) {
+  int *out =  (int*) malloc(n * m * sizeof(int));
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      
+    }
+  }
+  
+}
+
 /*
 Input: n * m * 3 array
 Output: Processed seam
@@ -7,8 +36,6 @@ Output: Processed seam
 void host_generate_seam(int *in, int n, int m, int *out) {
   out =  (int*) malloc(3 * n * m * sizeof(int));
 
-
-    
 }
 
 
