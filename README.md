6aebd97aa444fd099e378f3a0e4d69740d216f18
greysvarlesp <tr4nquoc@gmail.com>
2023-01-03 01:55:17 +0700

    update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Seam carving
 
+Final project in HCMUS parallel programming course. An implementation of seam carving in CUDA.
+
 Tasks: [Link](https://docs.google.com/spreadsheets/d/1niNkgiatqVv5ZHbLDha_LVsxlsbOHn0Jkyx4X0cTt-Q/edit?usp=sharing)
 
 Notebook: [Link](https://colab.research.google.com/drive/1dG64BS-6aJgMvjVhSdZmtZ1NyFAy_ts9?usp=sharing)
-
--- src/gpu_v1.cu
+++ src/gpu_v1.cu
@@ -6,39 +6,48 @@ const int SOBEL_X[] = {
 
 const int SOBEL_Y[] = {1, 2, 1, 0, 0, 0, -1, -2, -1};
 
-__global__ void V1_conv_kernel(int *in, int n, int m, int *out) {
+__global__ void V1_conv_kernel(int *in, int n, int m, int *out) {}
 
-}
-
-void V1_conv(int *in, int n, int m, int *out) {
-
-
-}
-
+void V1_conv(int *in, int n, int m, int *out) {}
 
 /*
    Dynamic programming kernel for finding seam
-
    */
-
-__global__ void V1_dp_kernel(int *d_in, int *d_dp, int row, int col_size, int *d_out) {
+__global__ void V1_dp_kernel(int *d_in, int *d_dp, int *d_trace, int row,
+                             int col_size) {
 
   int col = blockIdx.x * blockDim.x + threadIdx.x;
 
-  if (col >= col_size) return;
+  if (col >= col_size)
+    return;
+  if (row == 0) {
+  }
 
   int ans = -1;
+  int tr = -1;
 
   for (int j = -1; j <= 1; ++j) {
     int col_ = col + j;
-    if (col_ < 0 || col_ >= col_size) continue;
-    
+    if (col_ < 0 || col_ >= col_size)
+      continue;
   }
+
+  d_trace[row * col_size + col] = tr;
   d_dp[row * col_size + col] = ans;
 }
 
+__global__ void seam_removal_kernel() {
 
-/* 
+}
+
+__global__ void seam_addition_kernel() {
+
+
+}
+
+
+
+/*
 Input: n * m energy map
 Output: result
 */
@@ -47,8 +56,5 @@ void V1_seam(int *in, int n, int m, int *out, int block_size = 256) {
   int grid_size = (m - 1) / block_size + 1;
 
   for (int i = 0; i < n; ++i) {
-
-
   }
-  
 }
