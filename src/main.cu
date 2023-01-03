d24d51c143d8478da64dc1cad7459f7c62f0dbfe
greysvarlesp <tr4nquoc@gmail.com>
2023-01-03 15:46:12 +0700

    change image reading

--- src/main.cu
+++ src/main.cu
@@ -10,15 +10,25 @@
 
 using std::cout;
 
-int main() {
-  printDeviceInfo();
+int main(int argc, char **argv) {
 
-  int width, height, channels;
-  unsigned char *img = stbi_load("sky.jpg", &width, &height, &channels, 0);
-  if (img == NULL) {
-    cout << "Error in loading the image\n";
-    exit(1);
+  int ver = 0;
+  if (argc > 2) {
+    ver = atoi(argv[1]);
   }
 
-  cout << "Loaded image with size of " << width << "x" << height << " channels " << channels << '\n';
+  if (ver > 0)
+    printDeviceInfo();
+
+  if (argc > 2) {
+    int width, height, channels;
+    unsigned char *img = stbi_load(argv[2], &width, &height, &channels, 0);
+    if (img == NULL) {
+      cout << "Error in loading the image\n";
+      exit(1);
+    }
+
+    cout << "Loaded image with size of " << width << "x" << height
+         << " channels " << channels << '\n';
+  }
 }
