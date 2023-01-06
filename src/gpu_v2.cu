ae0a3e2d31e1fdc1f277cf30d3e202af74e5d437
greysvarlesp <tr4nquoc@gmail.com>
2023-01-07 00:11:12 +0700

    Start on v2

--- src/gpu_v2.cu
+++ src/gpu_v2.cu
@@ -1,6 +1,7 @@
 #include "gpu_utils.h"
 #include "gpu_v1.h"
 #include "gpu_v2.h"
+#include <__clang_cuda_builtin_vars.h>
 #include <algorithm>
 #include <iostream>
 
@@ -19,10 +20,43 @@ __global__ void V2_grayscale_kernel(unsigned char *d_in, int num_pixels,
 }
 
 
+/*
+   Manual merge, Parallelized  DP.
+   Split into 4 parts
+   */
+
+__global__ void V2_conv_kernel(int *d_in, int height, int width, int *d_out) {
+
+}
+
+__device__ int bCount;
+__device__ int done;
+
+__global__ void V2_dp_kernel(int *d_in, int *d_out, int *d_dp) {
+  __shared__ int bi;
+
+  if (threadIdx.x == 0) {
+    bi = atomicAdd(&bCount, 1);
+  }
+
+  __syncthreads();
+
+  int cnt = gridDim.x * blockDim.x;
+  int row = bi / cnt;
+  int col = bi % cnt;
+
+
+
+
+}
+
+
 // overlapping convolution
 double V2_dp_seam() {
   GpuTimer timer;
   timer.Start();
+
+
 
 
   timer.Stop();
