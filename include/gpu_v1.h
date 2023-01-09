0ff1f0be9ccd726ff683a1219da7a2c8e32b4956
greysvarlesp <tr4nquoc@gmail.com>
2023-01-10 03:05:39 +0700

    fix header

--- include/gpu_v1.h
+++ include/gpu_v1.h
@@ -1,16 +1,15 @@
 #ifndef GPU_V1_H
-#define GPU_V1_H 
+#define GPU_V1_H
 
-
-void V1_conv(int *in, int w, int h, int *out, int block_size = 32);
-double V1_seam(int *in, int n, int m, int *out, int blocksize  = 256);
+void V1_conv(int *in, int height, int width, int *out, int block_size = 32);
+double V1_seam(int *in, int n, int m, int *out, int blocksize = 256);
 
 // 1024 max
-void V1_grayscale(unsigned char *in, int height, int width, int *out, int blocksize = 32);
-int V1_min(int* in, int* ind, int n, int block_size=32);
+void V1_grayscale(unsigned char *in, int height, int width, int *out,
+                  int blocksize = 32);
+int V1_min(int *in, int *ind, int n, int block_size = 32);
 
 __global__ void V1_grayscale_kernel(unsigned char *d_in, int height, int width,
                                     int *out);
 
 #endif /* GPU_V1_H */
-
